3ea474ec4b7a7666102c0ef0ab0a7437da1e89bf
Marcelo Mastroiani <mastroianimarcelo04@gmail.com>
2023-09-06 22:50:13 -0300

    se agrego class

--- proyecto/js/prestamo.js
+++ proyecto/js/prestamo.js
@@ -1,78 +1,104 @@
 
-// FUNCIONES //
+// FUNCION //
+class planPrestamo{
 
-
-
-//FUNCION PARA CALCULAR EL INTERES DEL PRESTAMO//
-
-function calculo_del_prestamo( monto , cuotas ){
-
-    monto = parseFloat(monto);
-    cuotas = parseInt(cuotas);
-
-    let prestamo_final = 0;
-
-    if( cuotas == 3 && monto > 0){
-        prestamo_final = monto + (monto * 0.25);
-        return prestamo_final
+    constructor( plan , monto , cuotas , total , mes){
+        this.plan = plan;
+        this.monto = monto;
+        this.cuotas = cuotas;
+        this.total = total;
+        this.mes = mes;
     }
-    else if( cuotas == 6 && monto > 0 ){
-        prestamo_final = monto + (monto * 0.60);
-        return prestamo_final
+
+    get_datos(){
+        console.log("Tipo de plan: ", this.plan);
+        console.log("Monto pedido: ", this.monto)
+        console.log("Cuotas: ", this.cuotas);
+        console.log("Total a pagar: ", this.total);
+        console.log("Cuota por mes: ",this.mes);
+        console.log("");
     }
-    else if( cuotas == 12  && monto > 0){
-        prestamo_final = monto + (monto * 1);
-        return prestamo_final
+    get_interes(){
+        this.monto = parseFloat(this.monto);
+        this.cuotas = parseInt(this.cuotas)
+
+        if( this.cuotas == 6 && this.monto > 0 ){
+            this.total = this.monto + (this.monto * 0.50);
+            return this.total
+        }
+        else if( this.cuotas == 12 && this.monto > 0 ){
+            this.total = this.monto + (this.monto * 1);
+            return this.total
+        }
+        else if( this.cuotas == 24 && this.monto > 0 ){
+            this.total = this.monto + (this.monto * 2);
+            return this.total
+        }
+        if( this.cuotas == 36 && this.monto > 0 ){
+            this.total = this.monto + (this.monto * 3);
+            return this.total
+        }
+    }  
+    get_mes(){
+        this.mes = this.total / this.cuotas;
+    return this.mes 
     }
-    else if( cuotas == 24  && monto > 0){
-        prestamo_final = monto + (monto * 2);
-        return prestamo_final
-    }
-    
 }
 
-//FUNCION PARA SACAR LAS CUOTAS QUE SE DEVEN PAGAR POR MES//
+let montoEleguido = prompt("Ingrese el monto del prestamo que desee pedir");
 
-function cuotas_por_mes(resultado_del_prestamo , cuotas){
+//ARRAY DE LOS PLANES
+let listaDePlanes = [];
 
+listaDePlanes.push( new planPrestamo("PLAN 1" , montoEleguido , 6 ,  this.total , this.mes))
+listaDePlanes.push( new planPrestamo("PLAN 2" , montoEleguido , 12 , this.total , this.mes));
+listaDePlanes.push( new planPrestamo("PLAN 3" , montoEleguido , 24 , this.total , this.mes));
+listaDePlanes.push( new planPrestamo("PLAN 4" , montoEleguido , 36 , this.total , this.mes));
 
-    let cuotas_mes = 0;
-    
-    cuotas_mes = resultado_del_prestamo / cuotas;
-    return cuotas_mes 
+//OPERACION PARA CALCULAR EL INTERES DEL PRESTAMO
+for(let sumaInteres of listaDePlanes){
 
+    sumaInteres.get_interes();
+}
+
+//OPERACION PARA SACAR LAS CUOTAS QUE SE DEVEN PAGAR POR MES
+for(let porMes of listaDePlanes){
+
+    porMes.get_mes();
+}
+
+//DATOS DE LOS PLANES
+console.log("LISTA DE PLANES");
+
+for(let planPrestamo of listaDePlanes){
+
+    planPrestamo.get_datos();
+}
+
+// ELECCION DE PLAN //
+function buscarPlan( planPrestamo ){
+
+    return planPrestamo.plan == eleguiPlan
 
 }
 
+let eleguiPlan = "";
 
+while( eleguiPlan != "FIN"){
+    eleguiPlan = prompt("Ingrese el nombre del plan que desee eleguir o FIN");
 
-// SIMULADOR //
-
-let monto = 0;
-
-console.log("¡Bienvenido/a a nuestra pagina de prestamos!")
-
-while( monto != "FIN" ){
-
-    monto = prompt("Ingrese el monto que desea solicitar o FIN");
-
-    if( monto != "FIN"){
-
-        let cuotas = prompt("Ingrese la cantidad de cuotas: 3, 6, 12 y 24");
-        let resultado_del_prestamo = calculo_del_prestamo( monto , cuotas );
-        let resultado_cuotas_mes = cuotas_por_mes(resultado_del_prestamo , cuotas);
-
-        console.log("Pediste: ", monto);
-        console.log("Cuotas: ", cuotas);
-        console.log(cuotas,"cuotas de:",resultado_cuotas_mes);
-        console.log("Devolves el total de: ",resultado_del_prestamo );
+    if( eleguiPlan != "FIN"){
         
+        let resultado_find = listaDePlanes.find( buscarPlan );
 
+        if(resultado_find != undefined ){
 
+            console.log("Muchas gracias, usted eliguio:");
+            resultado_find.get_datos();
 
-    }
-    else{
-        console.log("Muchas gracias");
-    }
-
+        }
+        else{
+            console.log("No contamos con ese plan");
+        }
+    }  
 }
